5a80d0b30d95f3892c8ae52f46b1f725547bf8c2
Robert Pittman <robertamoah271@gmail.com>
2019-08-05 19:07:51 -0400

    started the front end try to funish it

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,33 @@
-// we start here///
+
+
+// FOURSQUARE API CALL//
+fetch('https://api.foursquare.com/v2/venues/explore?cat=food&near=paterson&client_id=MJ1WCPB5DROKCZ54T0U0F1RLR3JJIEEFOBMPRVU0DECNRAQJ&client_secret=3BYYGW4CSRPFTLJZRIXG1XEE2Q4DARJUZA24YRN4OPO1K0NO&v=20180323&limit=40')
+.then(resp => resp.json())
+.then(result  => console.log(result));
+
+
+
+
+
+// /////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+//GOOGLE MAPS API CAL//
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+// /////////////////////////////////////////////////////////////////
+
+
+
---
+++ assets/javascript/search.js
@@ -0,0 +1,4 @@
+function activatePlacesSearch(){
+var input = document.getElementById("search_term1");
+var autocomplete = new google.maps.places.Autocomplete(input);
+}
