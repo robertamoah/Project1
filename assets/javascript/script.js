31e650f12f3acb38106a90edc6eb50615bbd9b8c
Robert Pittman <robertamoah271@gmail.com>
2019-08-08 19:05:07 -0400

    added api autocomplete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,22 @@
-// we start here///
+
+
+// FOURSQUARE API CALL//
+fetch('https://api.foursquare.com/v2/venues/explore?cat=food&near=paterson&client_id=MJ1WCPB5DROKCZ54T0U0F1RLR3JJIEEFOBMPRVU0DECNRAQJ&client_secret=3BYYGW4CSRPFTLJZRIXG1XEE2Q4DARJUZA24YRN4OPO1K0NO&v=20180323&limit=40')
+.then(resp => resp.json())
+.then(result  => console.log(result));
+
+
+
+
+
+//GOOGLE MAPS API CALL//
+var map;
+function createMap(){
+  var options ={
+    center:{lat:-33.867, lng:151.195},
+    zoom: 10
+  };
+  map= new google.maps.Map(document.getElementById("map"), options)
+};
+
+
---
+++ assets/javascript/search.js
@@ -0,0 +1,12 @@
+
+function activatePlacesSearch(){
+    var input = document.getElementById("autoComplete")
+    var autocomplete = new google.maps.places.Autocomplete(input)
+}
+
+
+
+
+
+
+
